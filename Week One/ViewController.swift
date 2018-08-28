d97790e46c1ebb77a007394e26b2e298ffd09b2d
Nicholas Cannon <nicholasobriencannon@nicholass-macbook-pro.local>
2018-08-28 17:32:38 -0400

    Initial Commit

---
+++ Week One/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week One
+//
+//  Created by Nicholas O'Brien Cannon on 8/28/18.
+//  Copyright © 2018 Nicholas O'Brien Cannon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
